327f56ec5bc14136c433e41b2f9c014f458801de
Aleksi Puttonen <aleksi.puttonen@gmail.com>
2024-06-24 22:41:23 +0300

    Initial Commit

---
+++ SoftAwake/SoftAwakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoftAwakeApp.swift
+//  SoftAwake
+//
+//  Created by Aleksi Puttonen on 24.6.2024.
+//
+
+import SwiftUI
+
+@main
+struct SoftAwakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
