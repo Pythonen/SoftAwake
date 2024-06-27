42e52890f9226bc80475e40c753e0e40f42380d9
Aleksi Puttonen <aleksi.puttonen@gmail.com>
2024-06-27 20:45:20 +0300

    Add button to go to alarm list

--- SoftAwake/ClockView.swift
+++ SoftAwake/ClockView.swift
@@ -10,17 +10,28 @@ import SwiftUI
 struct ClockView: View {
     var body: some View {
         NavigationStack {
-            
-        VStack {
-            HStack {
-                Image(systemName: "alarm")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Set a time for soft awakening")
+            VStack {
+                HStack {
+                    Image(systemName: "alarm")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    Text("Set a time for soft awakening")
+                }
+                .padding()
+                ClockInput()
+                NavigationLink(destination: AlarmListView()) {
+                    HStack {
+                        Image(systemName: "arrow.right.circle.fill")
+                        Text("Or go to your alarms")
+                    }
+                    .padding(5)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                }
+                .tint(Color.blue.opacity(0.7))
+                .buttonStyle(.borderedProminent)
+                .padding()
             }
-            .padding()
-            ClockInput()
-        }
         }
     }
 }
